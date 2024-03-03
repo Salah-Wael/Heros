9989d3d5ace750957cea280fe385d2031b83048a
Salah EL-Din <sasa.wael2016@gmail.com>
2024-03-03 02:41:41 +0200

    modife the admin nav and index&all files

---
+++ .history/resources/views/news/all.blade_20240302203129.php
@@ -0,0 +1,60 @@
+@extends('news.index')
+@section('title')
+Heros | all News
+@endsection
+
+@section('css')
+    @parent
+@endsection
+
+@section('admin-nav')
+    @if (auth()->user()->role == 'admin')
+        @parent
+    @endif
+@endsection
+
+{{-- @section('content')
+@if (session('success'))
+    <div style="height:40px;color:black;background-image: linear-gradient(to right,#DF63FF,#82E9EF);display: flex;align-items: center;justify-content: center;">
+        {{ session('success')  }}
+    </div>
+    {!! '<br><br>' !!}
+    @endif --}}
+{{-- <div class="news">
+    @if ($news)
+        @foreach($news as $new)
+            <div class="card mb-3 news_card">
+                <strong>
+                    <a href="/user">
+                        {{ 'By '. $new->firstName.' '.$new->lastName }}
+                    </a>
+                </strong>
+                @if ($new->created_at == $new->updated_at)
+                    {{ ", created at" }} {{ date("Y-m-d h:m",strtotime($new->created_at)) }}
+                @else
+                    {{ ", updated at" }} {{ date("Y-m-d h:m",strtotime($new->updated_at)) }}
+                @endif
+                <b>{{$new->title }}</b>
+                <br>
+                <img src="{{ asset("assets/images/news/".$new->image) }}" alt="{{ $new->title }}">
+                <p >{!! nl2br($new->content) !!}</p>
+                <a class="btn btn-primary" href="{{ route('news.show',$new->id) }}" role="button">Read</a>
+                <a class="btn btn-primary" href="{{ route('news.edit',$new->id) }}" role="button">Edit</a>
+                <form action="{{ route('news.delete',$new->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-danger" name="post-delete-form" value="Delete">
+                </form>
+            </div> --}}
+            @section('buttons')
+                <a class="btn btn-primary" href="{{ route('news.edit',$new->id) }}" role="button">Edit</a>
+                <form action="{{ route('news.delete',$new->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-danger" name="post-delete-form" value="Delete">
+                </form>
+            @endsection
+        {{-- @endforeach
+    @endif
+</div>
+@endsection --}}
